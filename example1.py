af5a381a64c3fc08053f030578bb546a39bad200
P. L. Lim <2090236+pllim@users.noreply.github.com>
2018-08-03 16:14:10 -0400

    Clean up example 1

--- example1.py
+++ example1.py
@@ -1,23 +1,25 @@
 
-from astropy import coordinates as coords, units as u, wcs
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.wcs import WCS
 from astroquery.skyview import SkyView
 from astroquery.vizier import Vizier
 import matplotlib.pyplot as plt
 
-center = coords.SkyCoord.from_name('Orion KL')
+center = SkyCoord.from_name('Orion KL')
 
 # Grab an image from SkyView of the Orion KL nebula region
 imglist = SkyView.get_images(position=center, survey='2MASS-J')
 
-# the returned value is a list of images, but there is only one
+# The returned value is a list of images, but there is only one
 img = imglist[0]
 
 # 'img' is now a fits.HDUList object; the 0th entry is the image
-mywcs = wcs.WCS(img[0].header)
+mywcs = WCS(img[0].header)
 
 fig = plt.figure(1)
-fig.clf() # just in case one was open before
-# use astropy's wcsaxes tool to create an RA/Dec image
+fig.clf()  # Just in case one was open before
+# Use astropy's wcsaxes tool to create an RA/Dec image
 ax = fig.add_axes([0.15, 0.1, 0.8, 0.8], projection=mywcs)
 ax.set_xlabel("RA")
 ax.set_ylabel("Dec")
@@ -25,27 +27,29 @@ ax.set_ylabel("Dec")
 ax.imshow(img[0].data, cmap='gray_r', interpolation='none', origin='lower',
           norm=plt.matplotlib.colors.LogNorm())
 
-# retrieve a specific table from Vizier to overplot
-tablelist = Vizier.query_region(center, radius=5*u.arcmin, catalog='J/ApJ/826/16/table1')
-# again, the result is a list of tables, so we'll get the first one
+# Retrieve a specific table from Vizier to overplot
+tablelist = Vizier.query_region(
+    center, radius=5*u.arcmin, catalog='J/ApJ/826/16/table1')
+# Again, the result is a list of tables, so we'll get the first one
 result = tablelist[0]
 
-# convert the ra/dec entries in the table to astropy coordinates
-tbl_crds = coords.SkyCoord(result['RAJ2000'], result['DEJ2000'],
-                           unit=(u.hour, u.deg), frame='fk5')
+# Convert the ra/dec entries in the table to astropy coordinates
+tbl_crds = SkyCoord(result['RAJ2000'], result['DEJ2000'],
+                    unit=(u.hour, u.deg), frame='fk5')
 
-# we want this table too:
-tablelist2 = Vizier(row_limit=10000).query_region(center, radius=5*u.arcmin, catalog='J/ApJ/540/236')
+# We want this table too:
+tablelist2 = Vizier(row_limit=10000).query_region(
+    center, radius=5*u.arcmin, catalog='J/ApJ/540/236')
 result2 = tablelist2[0]
-tbl_crds2 = coords.SkyCoord(result2['RAJ2000'], result2['DEJ2000'],
-                            unit=(u.hour, u.deg), frame='fk5')
+tbl_crds2 = SkyCoord(result2['RAJ2000'], result2['DEJ2000'],
+                     unit=(u.hour, u.deg), frame='fk5')
 
-# overplot the data in the image
+# Overplot the data in the image
 ax.plot(tbl_crds.ra, tbl_crds.dec, '*', transform=ax.get_transform('fk5'),
         mec='b', mfc='none')
 ax.plot(tbl_crds2.ra, tbl_crds2.dec, 'o', transform=ax.get_transform('fk5'),
         mec='r', mfc='none')
-# zoom in on the relevant region
+# Zoom in on the relevant region
 ax.axis([100,200,100,200])
 
 plt.show()
