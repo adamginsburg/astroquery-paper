98467976b59dc793ea06a300e0e2e8d576ebcf7c
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2018-10-20 16:10:58 -0600

    fix example importing

--- example1.py
+++ example1.py
@@ -7,15 +7,15 @@ from astroquery.skyview import SkyView
 from astroquery.vizier import Vizier
 import matplotlib.pyplot as plt
 
-center = SkyCoord.from_name('Orion KL')
+center = SkyCoord.from_name("Orion KL")
 
 # Grab an image from SkyView of the Orion KL nebula region
-imglist = SkyView.get_images(position=center, survey='2MASS-J')
+imglist = SkyView.get_images(position=center, survey="2MASS-J")
 
 # The returned value is a list of images, but there is only one
 img = imglist[0]
 
-# 'img' is now a fits.HDUList object; the 0th entry is the image
+# "img" is now a fits.HDUList object; the 0th entry is the image
 mywcs = WCS(img[0].header)
 
 fig = plt.figure(1)
@@ -25,31 +25,31 @@ ax = fig.add_axes([0.15, 0.1, 0.8, 0.8], projection=mywcs)
 ax.set_xlabel("RA")
 ax.set_ylabel("Dec")
 
-ax.imshow(img[0].data, cmap='gray_r', interpolation='none', origin='lower',
+ax.imshow(img[0].data, cmap="gray_r", interpolation="none", origin="lower",
           norm=plt.matplotlib.colors.LogNorm())
 
 # Retrieve a specific table from Vizier to overplot
 tablelist = Vizier.query_region(
-    center, radius=5*u.arcmin, catalog='J/ApJ/826/16/table1')
-# Again, the result is a list of tables, so we'll get the first one
+    center, radius=5*u.arcmin, catalog="J/ApJ/826/16/table1")
+# Again, the result is a list of tables, so we"ll get the first one
 result = tablelist[0]
 
 # Convert the ra/dec entries in the table to astropy coordinates
-tbl_crds = SkyCoord(result['RAJ2000'], result['DEJ2000'],
-                    unit=(u.hour, u.deg), frame='fk5')
+tbl_crds = SkyCoord(result["RAJ2000"], result["DEJ2000"],
+                    unit=(u.hour, u.deg), frame="fk5")
 
 # We want this table too:
 tablelist2 = Vizier(row_limit=10000).query_region(
-    center, radius=5*u.arcmin, catalog='J/ApJ/540/236')
+    center, radius=5*u.arcmin, catalog="J/ApJ/540/236")
 result2 = tablelist2[0]
-tbl_crds2 = SkyCoord(result2['RAJ2000'], result2['DEJ2000'],
-                     unit=(u.hour, u.deg), frame='fk5')
+tbl_crds2 = SkyCoord(result2["RAJ2000"], result2["DEJ2000"],
+                     unit=(u.hour, u.deg), frame="fk5")
 
 # Overplot the data in the image
-ax.plot(tbl_crds.ra, tbl_crds.dec, '*', transform=ax.get_transform('fk5'),
-        mec='b', mfc='none')
-ax.plot(tbl_crds2.ra, tbl_crds2.dec, 'o', transform=ax.get_transform('fk5'),
-        mec='r', mfc='none')
+ax.plot(tbl_crds.ra, tbl_crds.dec, "*", transform=ax.get_transform("fk5"),
+        mec="b", mfc="none")
+ax.plot(tbl_crds2.ra, tbl_crds2.dec, "o", transform=ax.get_transform("fk5"),
+        mec="r", mfc="none")
 # Zoom in on the relevant region
 ax.axis([100,200,100,200])
 
